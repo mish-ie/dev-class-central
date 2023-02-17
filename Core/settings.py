72c599ee4714989f40dc76f3b40ad58cf1b08db5
atienoobwanda <atienoobwanda@gmail.com>
2023-02-18 00:53:40 +0300

    changing the core language and timezone to Indian based time

--- Core/settings.py
+++ Core/settings.py
@@ -111,9 +111,12 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+# LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+# TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'hi'
+
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
