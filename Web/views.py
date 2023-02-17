a0b9ef23855865dd7fcdc2a3f95c8dd9eb7105e5
atienoobwanda <atienoobwanda@gmail.com>
2023-02-18 01:32:05 +0300

    translate model fails

--- Web/views.py
+++ Web/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render, get_object_or_404
-# from .models import 
+from googletrans import Translator
 
+def my_view(request):
+    text = "Find| your next course."
+    
+    
 def home(request):
-    return render(request, 'Home/Home.html')
+    text = "Find| your next course."
+    lang = 'hi'
+    translator = Translator()
+    translated_text = translator.translate(text, dest=lang).text
+    translated_text = re.sub(r'\|', ' ', translated_text)
+    
+    return render(request, 'Home/Home.html', {'translated_text': translated_text})
