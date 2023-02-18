5af505ec737284e145a9a5f667cf5bd842dc8ffb
atienoobwanda <atienoobwanda@gmail.com>
2023-02-18 20:27:39 +0300

    fixing indentation error

--- Web/views.py
+++ Web/views.py
@@ -6,70 +6,66 @@ from django.utils.translation import get_language,activate,gettext
 
     
 def home(request):
-    trans=translate(language='hi')
-    return render(request, 'Home/Home.html',{'trans':trans})
+   #  trans=translate(language='hi')
+   return render(request, 'Home/Home.html')
  
- def Courses(request):
-    return render(request, 'Courses/Courses.html')
+def Courses(request):
+   return render(request, 'Courses/Courses.html')
  
- def Course(request):
-    return render(request, 'Courses/Course.html')
+def Course(request):
+   return render(request, 'Courses/Course.html')
+
  
+def Institutions(request):
+   return render(request, 'Institutions/Institutions.html')
  
- 
- def Institutions(request):
-    return render(request, 'Institutions/Institutions.html')
- 
- def Institution(request):
-    return render(request, 'Institutions/Institution.html')
- 
- 
- 
- def Rankings(request):
-    return render(request, 'Rankings/Rankings.html')
- 
- def Ranking(request):
-    return render(request, 'Rankings/Ranking.html')
+def Institution(request):
+   return render(request, 'Institutions/Institution.html')
  
  
  
- def Collections(request):
-    return render(request, 'Collections/Collections.html')
+def Rankings(request):
+   return render(request, 'Rankings/Rankings.html')
  
- def Collection(request):
-    return render(request, 'Collections/Collection.html')
- 
- 
- 
- def Subjects(request):
-    return render(request, 'Subjects/Subjects.html')
- 
-  def Subject(request):
-    return render(request, 'Subjects/Subject.html')
+def Ranking(request):
+   return render(request, 'Rankings/Ranking.html')
  
  
  
- def University(request):
-    return render(request, 'University/University.html')
+def Collections(request):
+   return render(request, 'Collections/Collections.html')
  
-  def Uni(request):
-    return render(request, 'University/Uni.html')
+def Collection(request):
+   return render(request, 'Collections/Collection.html')
+  
+def Subjects(request):
+   return render(request, 'Subjects/Subjects.html')
+
+def Subject(request):
+   return render(request, 'Subjects/Subject.html')
+ 
+  
+def University(request):
+   return render(request, 'University/University.html')
+ 
+def Uni(request):
+   return render(request, 'University/Uni.html')
  
  
  
- def Providers(request):
-    return render(request, 'Providers/Providers.html')
+def Providers(request):
+   return render(request, 'Providers/Providers.html')
  
-  def Provider(request):
-    return render(request, 'Providers/Provider.html')
+def Provider(request):
+   return render(request, 'Providers/Provider.html')
  
  
  
- def Reports(request):
-    return render(request, 'Reports/Reports.html')
+def Reports(request):
+   return render(request, 'Reports/Reports.html')
  
-  def Report(request):
-    return render(request, 'Reports/Report.html')
+def Report(request):
+   return render(request, 'Reports/Report.html')
  
  
  
