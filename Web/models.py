7187dd963f01274238117368a14f206cc01869f9
atienoobwanda <atienoobwanda@gmail.com>
2023-02-18 13:26:41 +0300

    defining models

--- Web/models.py
+++ Web/models.py
@@ -1,31 +1,69 @@
 from django.db import models
 
-class Stream(models.Model):
+class Ranking(models.Model):
     name = models.CharField(max_length=255)
+    description = models.TextField()
+    rank = models.IntegerField()
+    year = models.IntegerField()
+    image = models.ImageField(upload_to='rankings/', null=True, blank=True)
+
+class Collection(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    courses = models.ManyToManyField('Course')
+    image = models.ImageField(upload_to='collections/', null=True, blank=True)
+
+class Subject(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField()
+    courses = models.ManyToManyField('Course')
+    image = models.ImageField(upload_to='subjects/', null=True, blank=True)
+
+class University(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField()
+    courses = models.ManyToManyField('Course')
+    logo = models.ImageField(upload_to='universities/', null=True, blank=True)
+
+class Institution(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField()
+    courses = models.ManyToManyField('Course')
+    logo = models.ImageField(upload_to='institutions/', null=True, blank=True)
+
+class Provider(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField()
+    courses = models.ManyToManyField('Course')
+    logo = models.ImageField(upload_to='providers/', null=True, blank=True)
+
+class Report(models.Model):
+    title = models.CharField(max_length=255)
+    author_name = models.CharField(max_length=255)
+    author_profile_picture = models.ImageField(upload_to='authors/', null=True, blank=True)
+    publication_date = models.DateField()
+    url = models.URLField()
+    summary = models.TextField()
+    image = models.ImageField(upload_to='reports/', null=True, blank=True)
+
 
 class Course(models.Model):
     name = models.CharField(max_length=255)
-    stream = models.ForeignKey(Stream, on_delete=models.CASCADE)
-
-class Instructor(models.Model):
-    name = models.CharField(max_length=100)
-    homepage = models.TextField(null=True, blank=True)
-
-class Offering(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    slug = models.SlugField()
+    description = models.TextField()
+    url = models.URLField()
+    course_id = models.CharField(max_length=255)
+    workload = models.CharField(max_length=255, null=True, blank=True)
+    language = models.CharField(max_length=255, null=True, blank=True)
     start_date = models.DateField(null=True, blank=True)
     end_date = models.DateField(null=True, blank=True)
-    exact_dates_known = models.BooleanField(null=True, blank=True)
-    created = models.DateTimeField(null=True, blank=True, auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
-    url = models.TextField(null=True, blank=True)
-
-class CourseInstructor(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
-
-def trigger_set_created_column(sender, instance, **kwargs):
-    if not instance.created:
-        instance.created = timezone.now()
-
-models.signals.pre_save.connect(trigger_set_created_column, sender=Offering)
+    subject = models.ForeignKey('Subject', on_delete=models.CASCADE, null=True, blank=True)
+    university = models.ForeignKey('University', on_delete=models.CASCADE, null=True, blank=True)
+    institution = models.ForeignKey('Institution', on_delete=models.CASCADE, null=True, blank=True)
+    provider = models.ForeignKey('Provider', on_delete=models.CASCADE, null=True, blank=True)
+    image = models.ImageField(upload_to='courses/', null=True, blank=True)
+    
