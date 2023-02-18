abe9319d258530279c99fff504fb1062f1776a5a
atienoobwanda <atienoobwanda@gmail.com>
2023-02-18 16:17:46 +0300

    defining web app urls

--- Web/urls.py
+++ Web/urls.py
@@ -5,4 +5,28 @@ app_name = 'Web'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    
+    path('courses/', views.Courses, name='Courses'),
+    path('course/', views.Course, name='Course'),
+    
+    path('instituitions/', views.Institutions, name='Institutions'),
+    path('instituition/', views.Institution, name='Institution'),
+    
+    path('rankings/', views.Rankings, name='Rankings'),
+    path('ranking/', views.Ranking, name='Ranking'),   
+    
+    path('collections/', views.Collections, name='Collections'),
+    path('collection/', views.Collection, name='Collection'),
+    
+    path('subjects/', views.Subjects, name='Subjects'),
+    path('subject/', views.Subject, name='Subject'),
+    
+    path('university/', views.University, name='University'),
+    path('uni/', views.Uni, name='Uni'),
+    
+    path('providers/', views.Providers, name='Providers'),
+    path('provider/', views.Provider, name='Provider'), 
+    
+    path('reports/', views.Reports, name='Reports'),
+    path('report/', views.Report, name='Report'),
 ]
